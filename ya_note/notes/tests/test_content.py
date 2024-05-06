8d7699d47798c4d118c002a5882787e6827e6c49
Muhammad Saidov <saidov.m.05.01@gmail.com>
2024-05-06 08:46:51 +0300

    Wrote tests for site content. Unittest.

---
+++ ya_note/notes/tests/test_content.py
@@ -0,0 +1,45 @@
+from http import HTTPStatus
+
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from notes.models import Note
+from notes.forms import NoteForm
+
+User = get_user_model()
+
+
+class TestRoutes(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.author = User.objects.create(username='author')
+        cls.reader = User.objects.create(username='Reader')
+        cls.note = Note.objects.create(title='1', text='2', author=cls.author)
+
+    def test_notes_list_for_different_users(self):
+        users_statuses = (
+            (self.author, True),
+            (self.reader, False)
+        )
+        url = reverse('notes:list')
+        for user, note_in_list in users_statuses:
+            with self.subTest(user=user):
+                self.client.force_login(user)
+                response = self.client.get(url)
+                object_list = response.context['object_list']
+                self.assertEqual((self.note in object_list), note_in_list)
+
+    def test_pages_contains_form(self):
+        url_names = (
+            ('notes:add', None),
+            ('notes:edit', (self.note.id,))
+        )
+        self.client.force_login(self.author)
+        for name, args in url_names:
+            with self.subTest(name=name):
+                url = reverse(name, args=args)
+                response = self.client.get(url)
+                self.assertIn('form', response.context)
+                self.assertIsInstance(response.context['form'], NoteForm)
