35076bf8a822c89a214aaeccfcb40321055561ae
Muhammad Saidov <saidov.m.05.01@gmail.com>
2024-05-06 19:43:51 +0300

    Final project

--- ya_note/notes/tests/test_logic.py
+++ ya_note/notes/tests/test_logic.py
@@ -1,8 +1,9 @@
-from http import HTTPStatus
-
 from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
 from django.urls import reverse
+
+from http import HTTPStatus
+
 from pytils.translit import slugify
 
 from notes.models import Note
